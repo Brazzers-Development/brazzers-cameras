2522f89ae4c94d16a64b19a30e687b3d61dee812
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2023-04-01 19:20:11 -0400

    many config options added

--- shared/shared.lua
+++ shared/shared.lua
@@ -1,10 +1,18 @@
 Config = Config or {}
 
+Config.Core = 'qb-core'
 Config.Target = 'rush-eye'
+
+Config.RenewedPhone = true -- [true]: you're using the phone app instead of the standalone built UI
+Config.EnableCommand = false -- this is only for standalone version if you wanna enable a command to open the menu
+Config.Command = 'cameras'
+
+Config.DeleteOnDays = true -- [true]: allows cameras to be deleted from the database after the amount of days set below
+Config.DaysLasts = 7 -- [int] Above must be set to `true` for this to even do anything
 
 Config.Models = {
     ['prop_cctv_cam_06a'] = {
-        ['offset'] = vector3(0, 0.5, 0),
+        ['offset'] = vector3(0, 0, 0),
         ['allow-movement'] = true,
         ['max-rotation'] = 90,
         ['use-camera-filter'] = true,
@@ -27,4 +35,21 @@ Config.Models = {
         ['use-nui-filter'] = true,
         ['item'] = 'camera3',
     },
+}
+
+Config.Lang = {
+    ['primary'] = {
+        ['placed'] = 'Camera placed',
+        ['removed'] = 'Camera removed',
+        ['added'] = 'added to',
+        ['removedfrom'] = 'removed from camera',
+        ['altername'] = 'Only the owner can alter the camera name',
+        ['namechanged'] = 'Camera name changed to',
+        ['marked'] = 'Camera marked on your GPS',
+    },
+    ['error'] = {
+        ['stateid'] = 'State ID doesn\'t exist',
+        ['canceled'] = 'Canceled',
+        ['alreadyaccess'] = 'already has access to this camera',
+    }
 }
